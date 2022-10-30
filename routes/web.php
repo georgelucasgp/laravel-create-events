4962283949cab79310f238b29ca770374c743194
georgelucasgp <djgeorgelucas@hotmail.com>
2022-10-30 01:56:27 -0300

    relation one to many

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [EventController::class, 'index' ]);
-Route::get('/events/create', [EventController::class, 'create' ]);
+Route::get('/events/create', [EventController::class, 'create' ])->middleware('auth');
 Route::get('/events/{id}', [EventController::class, 'show' ]);
 Route::post('/events', [EventController::class, 'store' ]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
